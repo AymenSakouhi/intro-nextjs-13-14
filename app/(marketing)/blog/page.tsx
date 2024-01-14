16530db51f38c6c8bec2014309e9cdab0d0ab78b
Aymen Sakouhi <aymenmarketer@gmail.com>
2024-01-14 23:10:49 +0100

    How to use server side rendering with async.

--- app/(marketing)/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Blog = () => {
-  return <div>Blog page</div>
-}
-
-export default Blog
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  return { data: [1, 2, 3] }
+}
+
+export default async function Home() {
+  const data = await getData()
+  console.log(data)
   return <div>Home page</div>
 }
